62a3c9a55682c92869598148485a93263c7d0bef
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-12-01 12:49:51 +0530

    Fix configs

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,7 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
+import express from 'express';
+const app = express();
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-exports.default = app;
+export default app;
--- dist/server.js
+++ dist/server.js
@@ -1,10 +1,5 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
+import app from './app.js';
 const port = 3000;
-app_1.default.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
